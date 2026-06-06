b313a35c57f5be3af29e1fcd1ad29327d195cab8
JRonnie <ronaldjjuuko7@gmail.com>
2026-06-07 02:09:34 +0300

    post content fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -434,9 +434,10 @@ document.addEventListener('DOMContentLoaded', () => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     entry.target.classList.add('visible');
+                    observer.unobserve(entry.target);
                 }
             });
-        }, { threshold: 0.15, rootMargin: '0px 0px -50px 0px' });
+        }, { threshold: 0, rootMargin: '0px 0px -50px 0px' });
         animEls.forEach(el => observer.observe(el));
     }
 
